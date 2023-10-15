0b5502a15e9ba431c4406643f7134dc4c00b591d
tanish <tbhatia0225@gmail.com>
2023-10-15 18:15:31 +0530

    day 2

---
+++ Pattern Questions/inveetedhollowpyramid.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    for(int i=0; i<6;i++){
+        for(int j=0;j<6;j++){
+            if(i==0|| j==0 || j==6-i-1){
+                cout<<"* ";
+            }
+           
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+}
